aa36f04de892b40b53512472615d7bd8de8f04df
dosamhms <dosam@hermeszsoft.hu>
2019-09-25 13:09:16 +0200

    dist build

---
+++ dist/react-sortable-hoc.js
@@ -0,0 +1,25 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var index_js = _interopDefault(require('./SortableContainer/index.js'));
+var index_js$1 = _interopDefault(require('./SortableElement/index.js'));
+var index_js$2 = _interopDefault(require('./SortableHandle/index.js'));
+var utils_js = require('./utils.js');
+
+
+
+exports.SortableContainer = index_js;
+exports.sortableContainer = index_js;
+exports.SortableElement = index_js$1;
+exports.sortableElement = index_js$1;
+exports.SortableHandle = index_js$2;
+exports.sortableHandle = index_js$2;
+Object.defineProperty(exports, 'arrayMove', {
+	enumerable: true,
+	get: function () {
+		return utils_js.arrayMove;
+	}
+});
